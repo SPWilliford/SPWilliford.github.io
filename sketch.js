a0ad6bd63d658eac1f6225066cbbcb3d131cb0b9
Samuel Williford <HorseSong@protonamail.com>
2021-03-28 16:48:53 -0500

    changed header and made the canvas rainbow

--- sketch.js
+++ sketch.js
@@ -5,6 +5,21 @@ function setup() {
 
 function draw() {
   noStroke();
-  fill(255, 255,255,30);
-  circle(mouseX, mouseY, random(10,75));
+  fill(random(0,255),random(0,255),random(0,255),5);
+
+
+  let xPos = [];
+  let yPos = [];
+
+  for (let i = 0; i < 1000; i++)
+  {
+    xPos[i] = random(0,width);
+    yPos[i] = random(0,height);
+  }
+  for (let i = 0; i < 1000; i++)
+  {
+    circle(xPos[i],yPos[i], random(10,90));
+  }
+
+
 }
