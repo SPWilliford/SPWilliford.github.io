5679e1aea682489eba3bf61b506634cdf992211f
Samuel Williford <HorseSong@protonamail.com>
2021-03-30 11:53:41 -0500

    forgot to commit the js last time, moved the fullscreen button

--- sketch.js
+++ sketch.js
@@ -5,16 +5,16 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1200, 1800);
+  createCanvas(1200, windowHeight);
   textFont(myFont);
   textSize(100);
-  textAlign(CENTER);
+  textAlign(CENTER, BOTTOM);
   background(0);
 }
 
 function mousePressed()
 {
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < 100)
+  if (mouseX > 0 && mouseX < width && mouseY > height-100 && mouseY < height)
   {
     let fs = fullscreen();
     fullscreen(!fs);
@@ -25,9 +25,9 @@ function draw()
 {
   
   //button
-  rect(0,0,width,100);
+  rect(0,height-100,width,100);
   fill(random(0, 255), random(0, 255), random(0, 255),6);
-  text('FULLSCREEN',600,87);
+  text('FULLSCREEN',600,height);
   
   let xPos = [];
   let yPos = [];
